b2bca71464d885bd598cc435714761f1b4cf5192
Lucas Leal da Silva <lucasleal00100@gmail.com>
2026-06-11 13:09:58 -0300

    feat: componentizando o elemento Text

---
+++ src/components/Text/index.tsx
@@ -0,0 +1,16 @@
+import { StyleProp, Text, TextStyle } from "react-native";
+import { styles } from "./styles";
+
+type Props = {
+  children?: string;
+  className?: string;
+  style?: StyleProp<TextStyle>;
+};
+
+export const Title = ({ children, style }: Props) => {
+  return <Text style={[styles.title, style]}>{children}</Text>;
+};
+
+export const TextButton = ({ children, style }: Props) => {
+  return <Text style={[styles.textButton, style]}>{children}</Text>;
+};
---
+++ src/components/Text/styles.ts
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  title: {
+    fontWeight: 900,
+    margin: -4,
+    flexDirection: "column",
+    textAlign: "center",
+    fontSize: 40,
+    color: "#fff",
+    flexShrink: 1,
+  },
+  textButton: {
+    fontSize: 16,
+    fontWeight: "800",
+    textAlign: "center",
+  },
+});
