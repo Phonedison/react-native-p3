b23bc01c648a340e3ae58998e1c6fd1b1c0cf6dc
Lucas Leal da Silva <lucasleal00100@gmail.com>
2026-06-13 15:34:52 -0300

    feat: implementando component inputText

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,14 +1,55 @@
-import { TextInput } from "react-native";
+import { useState } from "react";
+import { Image, TextInput, TouchableOpacity, View } from "react-native";
+import { styles } from "./styles";
 
-type InputVariant = "user" | "password";
+type InputVariant = "text" | "password";
 type InputIcon = "look" | "email";
 
 type Props = {
   variant?: InputVariant;
-  children: string;
   icon?: InputIcon;
+  text?: string;
+};
+const iconMap = {
+  user: { uri: "https://www.svgrepo.com/show/524211/user.svg" },
+  passwordLock: {
+    uri: "https://www.svgrepo.com/show/524126/password-minimalistic-input.svg",
+  },
+  passwordUnlock: {
+    uri: "https://www.svgrepo.com/show/524127/password-minimalistic.svg",
+  },
 };
 
-export const Input = ({ variant }: Props) => {
-  return <TextInput></TextInput>;
+export const Input = ({ variant = "text", text }: Props) => {
+  const [isSecure, setIsSecure] = useState(variant === "password");
+
+  const toggle = () => {
+    setIsSecure((prevState) => !prevState);
+  };
+
+  const getIcon = () => {
+    if (variant !== "password") return iconMap.user;
+
+    return isSecure ? iconMap.passwordUnlock : iconMap.passwordLock;
+  };
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        placeholder={text}
+        style={styles.input}
+        secureTextEntry={isSecure}
+        textContentType={variant === "password" ? "password" : "none"}
+        keyboardType="default"
+      />
+
+      {variant === "password" ? (
+        <TouchableOpacity onPress={toggle} activeOpacity={0.7}>
+          <Image source={getIcon()} style={styles.icon} />
+        </TouchableOpacity>
+      ) : (
+        <Image source={getIcon()} style={styles.icon} />
+      )}
+    </View>
+  );
 };
--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,3 +1,27 @@
 import { StyleSheet } from "react-native";
 
-export const styles = StyleSheet.create({});
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    borderColor: "#cacaca86",
+    borderWidth: 0.5,
+    borderRadius: 16,
+    backgroundColor: "#66666641",
+    paddingVertical: 4,
+    paddingHorizontal: 24,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  input: {
+    width: "100%",
+    padding: 8,
+    color: "#ffffffb7",
+  },
+  icon: {
+    width: 20,
+    height: 20,
+    tintColor: "#cacaca86",
+  },
+});
